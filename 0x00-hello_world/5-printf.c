e8610911477c8c1a4d2b6ba6ea8df40a63e0114e
Anthony <anthonymacheda9@gmail.com>
2023-02-16 16:00:38 -0800

    Task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a c programm using printf
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
