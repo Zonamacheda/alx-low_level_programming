1f8efba113ed10af00723a036595d62af295c15a
Anthony <anthonymacheda9@gmail.com>
2023-02-21 02:24:02 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabe in lower case
+ */
+
+void print_alphabet(void);
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		a++;
+
+	-putchar('\n');
+}
