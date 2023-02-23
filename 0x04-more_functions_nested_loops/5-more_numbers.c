3a38d783d023000276cd774f9099708f0ac2d540
Anthony <anthonymacheda9@gmail.com>
2023-02-23 08:21:45 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers -> printing more numbers
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			_putchar(y + '0');
+		}
+		_putchar('\n');
+	}
+}
