ea681a60bcb16cee4cc1a745e8391bdbba7c6c4f
Anthony <anthonymacheda9@gmail.com>
2023-03-07 09:33:08 -0800

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,17 +8,6 @@
 
 int _evaluate(int i, int n)
 {
-/*Evaluate functions*/
-if (n == 0 || n == 1)
-return (n);
-
-else if (i * i < n)
-return (_evaluate(i + 1, n));
-
-else if (i * i == n) /*condition base*/
-return (i);
-
-return (-1);
-
-return (-1);
+	if ((root * root) == num)
+		return (root);
 }
