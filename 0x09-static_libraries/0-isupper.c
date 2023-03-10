4363de834f55276f9d262f9b80aade0c1ecfa4cb
Anthony <anthonymacheda9@gmail.com>
2023-03-10 08:22:04 -0800

    task0

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= '2')
+		return (1);
+	return (0);
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * _puts - writes strings to the output stream
+ *
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+
+	puts(" \"programming is like building a multilingual puzzle");
+	return (0);
+}
