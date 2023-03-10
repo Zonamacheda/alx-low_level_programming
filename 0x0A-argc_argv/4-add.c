458c58a1a590e01f95195ce4adc96860391ed721
Anthony <anthonymacheda9@gmail.com>
2023-03-10 14:39:11 -0800

    task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-
-/**
- * check_num - check -  string there are digit
- * @str: array str
- *
- * Return: Always 0 (sucess)
- */
-int check_num(char *str)
-{
-	/*Declaring variables*/
-	unsigned int count;
-
-	count = 0;
-	while (count < strlen(str)) /* count string */
-
-	{
-		if (!isdigit(str[count]))/* check if str there are digit */
-		{
-			return (0);
-		}
-
-		count++;
-	}
-	return (1);
-}
+#include <stlib.h>
 
 /**
  * main - print the name of the program
