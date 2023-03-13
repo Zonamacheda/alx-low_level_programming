c66d6f39351e7497d81d177c0459fab803db1fd4
Anthony <anthonymacheda9@gmail.com>
2023-03-13 08:26:30 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,38 +9,49 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int i = 0, len1 = 0, len2 = 0;
+	char *conct;
 
-	while (s1 && s1[len 1])
-		len++;
-	while (s2 && s2[len2])
-		len2++;
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
+	int i, ci;
+
+	if (s1 == NULL)
+
+		s1 = "";
+
+	if (s2 == NULL)
+
+		s2 = "";
+
+		i = ci = 0;
+
+	while (s2[ci] != '\0')
+
+		ci++;
+
+	conct = malloc(sizeof(char) * (i + ci + 1));
+
+	if (conct == NULL)
+
 		return (NULL);
 
-	i = 0;
-	j = 0;
+	i = ci = 0;
 
-	if (s1)
+	while (s1[i] != '\0')
 	{
-		while (i < len1)
-		{
-			s3[i] = s1[i];
-			i++;
-		}
+		conc[i] = s1[i];
+
+		i++;
+
 	}
 
-	if (s2)
+	while (s2[ci] != '\0')
 	{
-		while (i < (len1 + len2))
-		{
-			s3[i] = s2[j];
-			i++;
-			j++;
-		}
-		s3[i] = '\0';
+		conct[i] = s2[ci];
 
-		return (s3);
+		i++, ci++;
 	}
+
+	conct[i] = '\0';
+
+	return (conct);
+
+}
