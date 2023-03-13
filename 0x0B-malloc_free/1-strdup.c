bbc006a013baa0c51341e349dcfcd1663b2088a0
Anthony <anthonymacheda9@gmail.com>
2023-03-13 07:46:57 -0700

    task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,13 +15,17 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
+
 	while (str[len])
 		len++;
 	duplicate = malloc(sizeof(char) * (len + 1));
 
 	if (duplicate == NULL)
 		return (NULL);
+
 	while ((duplicate[i] = str[i]) != '\0')
 		i++;
+
 	return (duplicate);
 }
+
