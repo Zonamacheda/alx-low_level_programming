ef6a2ac487d1cbd0463e9d8544eb323b1335a301
Anthony <anthonymacheda9@gmail.com>
2023-03-16 04:15:20 -0700

    task 0

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,10 +16,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	size_t s1_len = strlen(s1);
 	size_t s2_len = strlen(s2);
 
-	if (n >= s2_len) n = s2_len;
+	if (n >= s2_len)
+	       	n = s2_len;
 	char *result = malloc(s1_len + n + 1);
 
-	if (result == NULL) return NULL;
+	if (result == NULL)
+	       	return NULL;
 
 	memcpy(result, s1, s1_len);
 	memcpy(result + s1_len, s2, n);
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 void *malloc_checked(unsigned int b);
-:int _putchar(char c);
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
