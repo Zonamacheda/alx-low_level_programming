3b0e42b4f00623619fbccc215bf987dde1b9b668
Anthony <anthonymacheda9@gmail.com>
2023-03-16 03:17:29 -0700

    task 0

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+void *malloc_checked(unsigned int b);
+:int _putchar(char c);
+void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif/*MAIN_H*/
