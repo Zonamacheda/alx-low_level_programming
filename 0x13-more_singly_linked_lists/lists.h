b1bba9acd0ae06fc4173df4150295f8dc5d9b07d
Anthony <anthonymacheda9@gmail.com>
2023-04-04 07:24:47 -0700

    fix

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -52,6 +52,7 @@ typedef struct listint_s
 int n;
 struct listint_s *next;
 } listint_t;
+int_putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
